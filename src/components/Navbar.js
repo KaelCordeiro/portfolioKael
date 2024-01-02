9fcc1541f9a4b4cf350bc1f9b1efad9e67ab022a
KaelCordeiro <kaellucaspinheirocordeiro@gmail.com>
2024-01-02 15:12:29 -0300

    Adicionando uma barra de menu

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import {Link} from "react-router-dom"
+import Container from "./Container"
+import styles from "./Navbar.module.css"
+import logo from "../..//src/img/computer-science.png"
+
+function Navbar(){
+    return(
+    <nav class={styles.navbar}>
+      <Container>
+        <Link to="/">
+            <img src={logo} alt="Computer" /> 
+        </Link>
+        <ul className={styles.list}>
+            <li className={styles.item}>
+                <Link to="/home">Home</Link>
+            </li>
+            <li className={styles.item}>
+                <Link to="/sobre">Sobre</Link>
+            </li>
+            <li className={styles.item}>
+                <Link to="/tecnologias">Tecnologias</Link>
+            </li>
+            <li className={styles.item}>
+                <Link to="/certificados">Certificados</Link>
+            </li>
+            <li className={styles.item}>
+                <Link to="/contato">Contatos</Link>
+            </li>
+        </ul>
+      </Container>
+    </nav>
+    )
+}
+
+export default Navbar
