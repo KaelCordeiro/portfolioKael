9bcda4e64fcf6a34f48357fff70baf4cf19ca947
KaelCordeiro <kaellucaspinheirocordeiro@gmail.com>
2024-01-02 15:09:20 -0300

    Página da Home

---
+++ src/home/Home.js
@@ -0,0 +1,11 @@
+import styles from "./Home.module.css"
+
+function Home(){
+    return(
+        <p className={styles.escrita}>
+            Kael Lucas Pinheiro Cordeiro
+        </p>
+    );
+}
+
+export default Home;
