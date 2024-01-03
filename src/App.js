518fd42214503cd55e2115369dc5f4e6b36d94ff
KaelCordeiro <kaellucaspinheirocordeiro@gmail.com>
2024-01-03 16:10:53 -0300

    Certificados

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import Container from "./components/Container";
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer";
+import Home from "./home/Home";
+import Sobre from "./sobre/Sobre";
+import Tecnologia from "./tecnologias/Tecnologia"
+    
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      return (
+       <Router>
+        <Navbar />
+        <Switch>
+          <Container>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route path="/sobre">
+            <Sobre />
+          </Route>
+          <Route path="/tecnologias">
+            <Tecnologia />
+          </Route>
+          </Container>
+        </Switch>
+        <Footer />
+       </Router>
+      );
+    }
 
 export default App;
---
+++ src/certificados/Certificado.js
@@ -0,0 +1,7 @@
+export function Certificado() {
+    return (
+        <div>
+            <img src="certificado_ingles.PNG" />
+        </div>
+    );
+}
