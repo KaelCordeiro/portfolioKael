64fee1c6efe80bef5eaef4159259ba27e9a3da0c
KaelCordeiro <kaellucaspinheirocordeiro@gmail.com>
2024-01-03 16:34:38 -0300

    Página de Certificados

--- src/certificados/Certificado.js
+++ src/certificados/Certificado.js
@@ -1,7 +1,25 @@
-export function Certificado() {
+import styles from "./Certificado.module.css"
+
+import ingles from "../img/certificado_ingles.jpg"
+import IPM2020 from "../img/IPM SISTEMAS 2020.jpg"
+import empreendedor from "../img/IF MAIS EMPREENDEDOR.jpg"
+import computacao from "../img/BCC.jpg"
+import IPM2023 from "../img/IPM SISTEMAS 2023.jpg"
+import rocket from "../img/rocketseat.jpg"
+import alura from "../img/alura.jpg"
+
+function Certificado() {
     return (
         <div>
-            <img src="certificado_ingles.PNG" />
+            <img className={styles.container} src={computacao}/>
+            <img className={styles.container} src={rocket}/>
+            <img className={styles.container} src={IPM2023}/>
+            <img className={styles.container} src={alura}/>
+            <img className={styles.container} src={ingles}/>
+            <img className={styles.container} src={IPM2020}/>
+            <img className={styles.container} src={empreendedor}/>
         </div>
     );
 }
+
+export default Certificado;
