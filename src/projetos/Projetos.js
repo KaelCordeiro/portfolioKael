078ebf864a28319e498d7cd43e1b29cd3b5894b7
KaelCordeiro <kaellucaspinheirocordeiro@gmail.com>
2024-01-05 14:29:45 -0300

    Atualização final da página de Projetos

--- src/projetos/Projetos.js
+++ src/projetos/Projetos.js
@@ -12,27 +12,31 @@ function Projetos(){
             <hr className={styles.linha}></hr>
             <div className={styles.container}>
                 <h3>Optimus Tech:</h3>
-                <p>Projeto de página para empresa fictícia OptimusTech, onde mostra as vagas abertas em busca do</p>
-                <p>Design e Desenvolvimento de Software, e o trabalho em equipe 100% remota com pessoas do Brasil inteiro.</p>
-                <p>Brasil inteiro.</p>
+                <p>Projeto de página para empresa fictícia OptimusTech, onde mostra vagas abertas para</p>
+                <p className={styles.linha_branco}>Design e Desenvolvimento de Software, 100% remoto.</p>
                 <div className={styles.container_imagem}>
                     <img src={optimus} />
                 </div>
-                <h3>Tecnologias Utilizadas:</h3>
+                <h4>Tecnologias Utilizadas:</h4>
                 <div className={styles.container_1}><FaHtml5 /></div>
                 <div className={styles.container_2}><IoLogoCss3 /></div>
+                <br></br>
+                <h3>Projeto:<a className={styles.link} href="https://kaelcordeiro.github.io/projetoSiteOptimusTech/" target="_blank" rel="noopener noreferrer">OptimusTech</a></h3>
+                <h3>Código fonte:<a className={styles.link} href="https://github.com/KaelCordeiro/projetoSiteOptimusTech" target="_blank" rel="noopener noreferrer">https://github.com/KaelCordeiro/projetoSiteOptimusTech</a></h3>
             </div>
+            <div className={styles.linha_traco}></div>
             <div className={styles.container}>
                 <h3>Clone Linkedin:</h3>
-                <p>Projeto foi desenvolvido pelo site da rede social Linkedin, onde o seu perfil vai conter</p>
-                <p>os seus destaques, atividades, sobre, experiências (pela empresa), formação acadêmica e idiomas.</p>
-                <p>Com essa rede social ajuda a buscar uma oportunidade para a sua vaga de emprego.</p>
-                <div className={styles.container_imagem}>
+                <p className={styles.linha_branco}>Projeto de clone da página perfil do Linkedin.</p>
+                <div className={styles.container_imagem1}>
                     <img src={linkedin} />
                 </div>
-                <h3>Tecnologias Utilizadas:</h3>
+                <h4>Tecnologias Utilizadas:</h4>
                 <div className={styles.container_1}><FaHtml5 /></div>
                 <div className={styles.container_2}><IoLogoCss3 /></div>
+                <br></br>
+                <h3>Projeto:<a className={styles.link} href="https://kaelcordeiro.github.io/projetoLinkedin/" target="_blank" rel="noopener noreferrer">Linkedin</a></h3>
+                <h3>Código fonte:<a className={styles.link} href="https://github.com/KaelCordeiro/projetoLinkedin" target="_blank" rel="noopener noreferrer">https://github.com/KaelCordeiro/projetoLinkedin</a></h3>
             </div>
         </div>
     );
