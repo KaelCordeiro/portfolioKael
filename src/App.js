41a266c923e928bfef32f4cb6f53f5377b4eaefb
KaelCordeiro <kaellucaspinheirocordeiro@gmail.com>
2024-01-05 17:28:07 -0300

    Atualização final do portfólio

--- src/App.js
+++ src/App.js
@@ -1,40 +1,27 @@
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
-
-import Container from "./components/Container";
-import Navbar from "./components/Navbar"
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./home/Home";
 import Sobre from "./sobre/Sobre";
-import Projetos from "./projetos/Projetos"
-import Certificado  from "./certificados/Certificado";
+import Projetos from "./projetos/Projetos";
+import Certificado from "./certificados/Certificado";
 import Contatos from "./contatos/Contatos";
-    
+
 function App() {
-      return (
-       <Router>
-        <Navbar />
-        <Switch>
-          <Container>
-          <Route exact path="/home">
-            <Home />
-          </Route>
-          <Route path="/sobre">
-            <Sobre />
-          </Route>
-          <Route path="/projeto">
-            <Projetos />
-          </Route>
-          <Route path="/certificados">
-            <Certificado />
-          </Route>
-          <Route path="/contato">
-            <Contatos />
-          </Route>
-          </Container>
-        </Switch>
-        <Footer />
-       </Router>
-      );
-    }
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/projeto" element={<Projetos />} />
+        <Route path="/certificados" element={<Certificado />} />
+        <Route path="/contato" element={<Contatos />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,36 @@
-import {Link} from "react-router-dom"
-import Container from "./Container"
-import styles from "./Navbar.module.css"
-import logo from "../..//src/img/computer-science.png"
+import React from "react";
+import { Link } from "react-router-dom";
+import Container from "./Container";
+import styles from "./Navbar.module.css";
+import logo from "../..//src/img/computer-science.png";
 
-function Navbar(){
-    return(
-    <nav class={styles.navbar}>
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
       <Container>
         <Link to="/">
-            <img src={logo} alt="Computer" /> 
+          <img src={logo} alt="Computer" />
         </Link>
         <ul className={styles.list}>
-            <li className={styles.item}>
-                <Link to="/home">Home</Link>
-            </li>
-            <li className={styles.item}>
-                <Link to="/sobre">Sobre</Link>
-            </li>
-            <li className={styles.item}>
-                <Link to="/projeto">Projetos</Link>
-            </li>
-            <li className={styles.item}>
-                <Link to="/certificados">Certificados</Link>
-            </li>
-            <li className={styles.item}>
-                <Link to="/contato">Contatos</Link>
-            </li>
+          <li className={styles.item}>
+            <Link to="/home">Home</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/sobre">Sobre</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/projeto">Projetos</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/certificados">Certificados</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/contato">Contatos</Link>
+          </li>
         </ul>
       </Container>
     </nav>
-    )
+  );
 }
 
-export default Navbar
+export default Navbar;
