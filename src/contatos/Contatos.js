e968b6c1f2ce5024006cbe43c3d6ccd1dd34f774
KaelCordeiro <kaellucaspinheirocordeiro@gmail.com>
2024-01-17 13:57:37 -0300

    Página de contatos final

--- src/contatos/Contatos.js
+++ src/contatos/Contatos.js
@@ -5,20 +5,24 @@ import { SiGmail } from "react-icons/si";
 
 function Contatos(){
     return(
-        <div>
-            <p className={styles.titulo}>Meus Contatos:</p>
-            <p className={styles.sub_social}>Instagram:</p>
-            <div className={styles.container_1}><FaInstagram /></div>
-            <p className={styles.descricao_contato}>@kael.cordeiro</p>
-            <p className={styles.sub_social1}>Linkedin:</p>
-            <div className={styles.container_2}><FaLinkedin /></div>
-            <p className={styles.descricao_contato}>Kael Cordeiro</p>
-            <p className={styles.sub_social2}>Gmail:</p>
-            <div className={styles.container_3}><SiGmail /></div>
-            <p className={styles.descricao_contato1}>kaellucaspinheirocordeiro@gmail.com</p>
-            <p className={styles.sub_social3}>Discord:</p>
-            <div className={styles.container_4}><FaDiscord /></div>
-            <p className={styles.descricao_contato2}>Kael Cordeiro - kael_cordeiro</p>
+        <div className={styles.container}>
+            <h3>Meus Contatos:</h3>
+            <p>Instagram:</p>
+            <div className={styles.container_1}>
+                <a href="https://www.instagram.com/kael.cordeiro/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+            </div>
+            <p>Linkedin:</p>
+            <div className={styles.container_2}>
+                <a href="https://www.linkedin.com/in/kael-cordeiro-2a4910296/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+            </div>
+            <p>Gmail:</p>
+            <div className={styles.container_3}>
+                <a href="mailto:kaellucaspinheirocordeiro@gmail.com" target="_blank" rel="noopener noreferrer"><SiGmail /></a>
+            </div>
+            <p>Discord:</p>
+            <div className={styles.container_4}>
+                <a href="https://discord.com/kael_cordeiro" target="_blank" rel="noopener noreferrer"><FaDiscord /></a>
+            </div>
         </div>
     );
 }
