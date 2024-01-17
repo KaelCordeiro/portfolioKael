252cce269c6900b5a95bac0d1df7b155d551ee40
KaelCordeiro <kaellucaspinheirocordeiro@gmail.com>
2024-01-17 14:01:37 -0300

    Descrição inicial da home

---
+++ src/home/Descricao.js
@@ -0,0 +1,14 @@
+import styles from "./Home.module.css"
+
+function Descricao(){
+    return(
+        <p className={styles.mini_descricao}>
+            Como bacharel em Ciências da Computação e programador júnior, mergulho de cabeça na paixão 
+            pela tecnologia. Meu portfólio reflete minha jornada em transformar linhas de código em 
+            soluções eficientes. Estou animado para contribuir com minha mentalidade inovadora e habilidades técnicas, 
+            buscando oportunidades empolgantes em sua equipe para crescermos juntos.
+        </p>
+    );
+}
+
+export default Descricao;
