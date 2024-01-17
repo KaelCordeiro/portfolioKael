9cc020b4fe532af97f344bc0873e8c482bb54f54
KaelCordeiro <kaellucaspinheirocordeiro@gmail.com>
2024-01-17 13:58:18 -0300

    Página de certificados com slide

--- src/certificados/Certificado.js
+++ src/certificados/Certificado.js
@@ -1,25 +1,37 @@
+import {React, useRef} from "react";
 import styles from "./Certificado.module.css"
 
-import ingles from "../img/certificado_ingles.jpg"
-import IPM2020 from "../img/IPM SISTEMAS 2020.jpg"
-import empreendedor from "../img/IF MAIS EMPREENDEDOR.jpg"
-import computacao from "../img/BCC.jpg"
-import IPM2023 from "../img/IPM SISTEMAS 2023.jpg"
-import rocket from "../img/rocketseat.jpg"
 import alura from "../img/alura.jpg"
+import computacao from "../img/BCC.jpg"
+import ingles from "../img/certificado_ingles.jpg"
+import empreendedor from "../img/IF MAIS EMPREENDEDOR.jpg"
+import rocketseat from "../img/rocketseat.jpg"
+import ipm2020 from "../img/IPM SISTEMAS 2020.jpg"
+import ipm2023 from "../img/IPM SISTEMAS 2023.jpg"
+import { GrPrevious, GrNext } from "react-icons/gr";
 
-function Certificado() {
+const Certificado = () => {
+
+    let containerImagemRef = useRef(null);
+
+    const imagens = [computacao, rocketseat, ipm2023, alura, ingles, ipm2020, empreendedor]
+
+    const prev = () => containerImagemRef.current.scrollLeft -= containerImagemRef.current.clientWidth;
+    const next = () => containerImagemRef.current.scrollLeft += containerImagemRef.current.clientWidth;
+
     return (
-        <div>
-            <img className={styles.container} src={computacao}/>
-            <img className={styles.container} src={rocket}/>
-            <img className={styles.container} src={IPM2023}/>
-            <img className={styles.container} src={alura}/>
-            <img className={styles.container} src={ingles}/>
-            <img className={styles.container} src={IPM2020}/>
-            <img className={styles.container} src={empreendedor}/>
-        </div>
+      <div className={styles.conteudo}>
+        <div className={styles.prev} onClick={prev}><GrPrevious/></div>
+            <div className={styles.slides} ref={containerImagemRef}>
+                {imagens.map(image => {return (<img src={image} />)})}
+            </div>
+        <div className={styles.next} onClick={next}><GrNext/></div>
+      </div>
+      
     );
-}
-
+  };
+  
 export default Certificado;
+
+
+
