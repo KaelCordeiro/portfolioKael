055eb749b8a8a4398b7ad9b8107dca860b9157bc
KaelCordeiro <kaellucaspinheirocordeiro@gmail.com>
2024-01-18 12:30:33 -0300

    Mudanças na página de contatos e o index.css

--- src/contatos/Contatos.js
+++ src/contatos/Contatos.js
@@ -7,21 +7,19 @@ function Contatos(){
     return(
         <div className={styles.container}>
             <h3>Meus Contatos:</h3>
-            <p>Instagram:</p>
-            <div className={styles.container_1}>
-                <a href="https://www.instagram.com/kael.cordeiro/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
-            </div>
-            <p>Linkedin:</p>
-            <div className={styles.container_2}>
-                <a href="https://www.linkedin.com/in/kael-cordeiro-2a4910296/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
-            </div>
-            <p>Gmail:</p>
-            <div className={styles.container_3}>
-                <a href="mailto:kaellucaspinheirocordeiro@gmail.com" target="_blank" rel="noopener noreferrer"><SiGmail /></a>
-            </div>
-            <p>Discord:</p>
-            <div className={styles.container_4}>
-                <a href="https://discord.com/kael_cordeiro" target="_blank" rel="noopener noreferrer"><FaDiscord /></a>
+            <div className={styles.centralizar}>    
+                <div className={styles.container_1}>
+                    <a href="https://www.instagram.com/kael.cordeiro/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+                </div>
+                <div className={styles.container_2}>
+                    <a href="https://www.linkedin.com/in/kael-cordeiro-2a4910296/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+                </div>
+                <div className={styles.container_3}>
+                    <a href="mailto:kaellucaspinheirocordeiro@gmail.com" target="_blank" rel="noopener noreferrer"><SiGmail /></a>
+                </div>
+                <div className={styles.container_4}>
+                    <a href="https://discord.com/kael_cordeiro" target="_blank" rel="noopener noreferrer"><FaDiscord /></a>
+                </div>
             </div>
         </div>
     );
